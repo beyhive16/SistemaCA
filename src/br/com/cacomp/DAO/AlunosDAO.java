a343cb6d448b574b17f4a96b85f3760993ce7e12
kel <raquel.melo.job@gmail.com>
2017-06-26 00:51:35 -0300

    Testes de Alunos

--- src/br/com/cacomp/DAO/AlunosDAO.java
+++ src/br/com/cacomp/DAO/AlunosDAO.java
@@ -2,10 +2,11 @@ package br.com.cacomp.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import br.com.cacomp.conexao.Conexao;
-import br.com.cacomp.domain.Administradores;
 import br.com.cacomp.domain.Alunos;
 
 public class AlunosDAO {
@@ -23,5 +24,77 @@ public class AlunosDAO {
 		comando.setInt(1, aln.getMatricula());
 		comando.setString(2, aln.getNome());
 		comando.executeUpdate();
+	}
+	
+	public void excluir(Alunos aln) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("DELETE FROM Alunos ");
+		sql.append("WHERE matricula = ? ");
+
+		Connection conexao = Conexao.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setInt(1, aln.getMatricula());
+		comando.executeUpdate();
+	}
+
+	public void editar(Alunos aln) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("UPDATE Alunos ");
+		sql.append("SET nome = ? ");
+		sql.append("WHERE matricula = ? ");
+
+		Connection conexao = Conexao.conectar();
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setInt(1, aln.getMatricula());
+		comando.setString(2, aln.getNome());
+		comando.executeUpdate();
+	}
+	
+	public ArrayList<Alunos> listar() throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT matricula, nome ");
+		sql.append("FROM Alunos ");
+		sql.append("ORDER BY nome ASC ");
+
+		Connection conexao = Conexao.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+
+		ResultSet resultado = comando.executeQuery();
+		ArrayList<Alunos> retorno = new ArrayList<Alunos>();
+
+		while (resultado.next()) {
+			Alunos aln = new Alunos();
+			aln.setMatricula(resultado.getInt("matricula"));
+			aln.setNome(resultado.getString("nome"));
+
+			retorno.add(aln);
+		}
+
+		return retorno;
+	}
+
+	public Alunos buscaPorCodigo(Alunos aln) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT matricula, nome ");
+		sql.append("FROM Alunos ");
+		sql.append("WHERE matricula = ? ");
+
+		Connection conexao = Conexao.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setInt(1, aln.getMatricula());
+
+		ResultSet resultado = comando.executeQuery();
+		Alunos retorno = null;
+
+		if (resultado.next()) {
+			retorno = new Alunos();
+			retorno.setMatricula(resultado.getInt("matricula"));
+			retorno.setNome(resultado.getString("nome"));
+		}
+
+		return retorno;
 	}
 }
--- src/br/com/cacomp/teste/AlunosDAOteste.java
+++ src/br/com/cacomp/teste/AlunosDAOteste.java
@@ -2,28 +2,70 @@ package br.com.cacomp.teste;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-
 import org.junit.Test;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import br.com.cacomp.conexao.Conexao;
+import br.com.cacomp.domain.Alunos;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import br.com.cacomp.conexao.Conexao;
 import br.com.cacomp.DAO.AlunosDAO;
+import br.com.cacomp.conexao.Conexao;
 import br.com.cacomp.domain.Alunos;
 
 public class AlunosDAOteste {
-
-	@Test
+	
 	public void salvar() throws SQLException{
 		Alunos aln1 = new Alunos();
 		
-		aln1.setMatricula(1053255);
-		aln1.setNome("Raquel Batista de Melo");
-
+		aln1.setMatricula(1041112);
+		aln1.setNome("Sidiney Araújo");
 		AlunosDAO alnDAO = new AlunosDAO();
 		try {
 			alnDAO.salvar(aln1);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.out.println("falha na inserção");
 			e.printStackTrace();
 		}
 	}
+	@Test
+	public void listar() throws SQLException{
+		
+		AlunosDAO alnDAO = new AlunosDAO();
+		try {
+			ArrayList<Alunos> lista = alnDAO.listar();
+			
+			for (Alunos a : lista){
+				System.out.println("Resultado: "+a.getNome());
+			}
+		} catch (SQLException e) {
+			System.out.println("falha na listagem");
+			e.printStackTrace();
+		}
+
+	}
 	
+	public void excluir() throws SQLException {
+		Alunos aln = new Alunos();
+		AlunosDAO alnDAO = new AlunosDAO();
+		
+		aln.setMatricula(1053255);
+		alnDAO.excluir(aln);
+	}
+	@Test
+	public void editar() throws SQLException {
+		Alunos aln = new Alunos();
+		aln.setMatricula(1058382);
+		aln.setNome("Jhon Wilker");
+		try{
+			AlunosDAO alnDAO = new AlunosDAO();
+		    alnDAO.editar(aln);
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+	}
 }
